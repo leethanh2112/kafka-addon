67e99a186af8ac98795cf6fbe43cb4c740d8ee34
leethanh2112 <leethanh21121986@gmail.com>
2020-03-25 15:36:02 +0700

    test

--- start-kafka.sh
+++ start-kafka.sh
@@ -146,4 +146,4 @@ if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
   eval "$CUSTOM_INIT_SCRIPT"
 fi
 
-exec "KAFKA_OPTS="-javaagent:/opt/kafka/prometheus/jmx_prometheus_javaagent-0.3.1.jar=7071:/opt/kafka/prometheus/kafka-0𤾄.yml"" "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/config/server.properties"
+exec KAFKA_OPTS="-javaagent:/opt/kafka/prometheus/jmx_prometheus_javaagent-0.3.1.jar=7071:/opt/kafka/prometheus/kafka-0-8-2.yml" "/opt/kafka_2.12-2.4.1/bin/kafka-server-start.sh" "-daemon" "/opt/kafka/config/server.properties"
